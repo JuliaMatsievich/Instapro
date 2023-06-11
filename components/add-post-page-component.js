b67bc2667a27fab7bc71a46872626295dc647bae
Julia Matsievich <juliamatsievich@mail.ru>
2023-06-11 17:04:28 +0500

    add html addPost

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -4,8 +4,22 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
+        <div class="form">
+          <h3 class="form-title">Добавить пост</h3>
+          <div class="form-inputs">
+            <div class="upload-image-container">
+              <div class="upload-image">
+                <label class="file-upload-label secondary-button">
+                  <input class="file-upload-input" type="file" style="displa:none">Выберите фото
+                </label>
+              </div>
+            </div>
+            <label> Опишите фотографию
+              <textarea class="input textarea" rows="4"></textarea>
+            </label>
+            <button id="add-button" class="button">Добавить</button>
+          </div>
+        </div>     
     </div>
   `;
 
